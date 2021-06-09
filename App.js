f65571cbd90e0046c5c9b479394da550217608be
Verestra <revo@Revos-MacBook-Pro.local>
2021-06-09 17:12:16 +0700

    Splash Screen

---
+++ App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Animated, StyleSheet, View, Image, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/screens/Login'
+function App () {
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    const {Navigator, Screen} = createStackNavigator();
+    const [splashScreenVisible, setSplashScreenVisible] = useState(true);
+    hideSplashScreen=()=>{  
+        setSplashScreenVisible(false);  
+      }
+      const fadeIn = () => {
+        // Will change fadeAnim value to 1 in 5 seconds
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true
+        }).start();
+      };
+        useEffect(() => {
+            fadeIn()
+            setTimeout(function(){  
+                hideSplashScreen();  
+              }, 5000); 
+          });
+    let splashScreen = (  
+        <View style={styles.splashScreenRootView}>  
+            <View style={styles.splashScreenChildView}>
+                <Animated.View style={{
+                    // Bind opacity to animated value
+                    opacity: fadeAnim }}>
+                <Text style={styles.textSplash}>Welcome To</Text> 
+                <Image source={require('./src/assets/img/arc_logo.png')} />
+            </Animated.View> 
+            </View>      
+        </View> )  
+    return (
+        <NavigationContainer style={styles.navigationContainer}>
+                  <Navigator headerMode={'none'}>
+                    {/* Auth Screen */}
+                    <Screen  name="Login" component={Login} />
+                    {/* Home Screen */}
+                  </Navigator>
+                  {(splashScreenVisible === true) ? splashScreen : null  }
+        </NavigationContainer>  
+    )
+}
+
+const styles = StyleSheet.create( {   
+        navigationContainer: {
+          backgroundColor: '#F9F9F9',
+        },
+        splashScreenRootView: {  
+            justifyContent: 'center',  
+            flex:1,   
+            position: 'absolute',  
+            width: '100%',  
+            height: '100%',
+          },  
+       
+        splashScreenChildView: {  
+            justifyContent: 'center',  
+            alignItems: 'center',  
+            backgroundColor: '#FFFFFF',  
+            flex:1,  
+        },
+        textSplash : {
+            fontFamily: 'NunitoSans-ExtraLightItalic',
+            textAlign: 'center',
+            fontSize: 20,
+            color: "#6379F4"
+        }
+    });
+
+export default App
---
+++ src/screens/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+function Login () {
+    return (
+        <View>
+            <Text>This Is Login Page</Text>
+        </View>
+    )
+}
+export default Login
