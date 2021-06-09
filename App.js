5d0d8635878fb7fdbd15543d9cc7db568d37e5a2
litha <lithaoctadelistia@gmail.com>
2021-06-09 23:26:43 +0700

    route top up

--- App.js
+++ App.js
@@ -1,87 +1,91 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { StatusBar,Animated, StyleSheet, View, Image, Text} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useEffect, useState, useRef} from 'react';
+import {StatusBar, Animated, StyleSheet, View, Image, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import Test from './src/screens/Test.js'
+import Test from './src/screens/Test.js';
 
-import Login from './src/screens/Login'
-import Home from './src/screens/Home'
+import Login from './src/screens/Login';
+import Home from './src/screens/Home';
+import TopUp from './src/screens/TopUp';
 
-function App () {
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const {Navigator, Screen} = createStackNavigator();
-    const [splashScreenVisible, setSplashScreenVisible] = useState(true);
-    hideSplashScreen=()=>{  
-        setSplashScreenVisible(false);  
-      }
-      const fadeIn = () => {
-        // Will change fadeAnim value to 1 in 5 seconds
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 2500,
-          useNativeDriver: true
-        }).start();
-      };
-        useEffect(() => {
-            fadeIn()
-            setTimeout(function(){  
-                hideSplashScreen();  
-              }, 5000); 
-          });
-    let splashScreen = (  
-        <View style={styles.splashScreenRootView}>
-            <StatusBar
-            animated={true}
-            hidden={true} />  
-            <View style={styles.splashScreenChildView}>
-                <Animated.View style={{
-                    // Bind opacity to animated value
-                    opacity: fadeAnim }}>
-                <Text style={styles.textSplash}>Welcome To</Text> 
-                <Image source={require('./src/assets/img/arc_logo.png')} />
-            </Animated.View> 
-            </View>      
-        </View> )  
-    return (
-        <NavigationContainer style={styles.navigationContainer}>
-                  <Navigator headerMode={'none'}>
-                    {/* Testing Screen For Navigation Only */}
-                    <Screen name="navigation-testing" component={Test} />
-                    {/* Home Screen */}
-                    <Screen name="Home" component={Home} />
-                    {/* Auth Screen */}
-                    <Screen  name="Login" component={Login} />
-                  </Navigator>
-                  {(splashScreenVisible === true) ? splashScreen : null  }
-        </NavigationContainer>  
-    )
+function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const {Navigator, Screen} = createStackNavigator();
+  const [splashScreenVisible, setSplashScreenVisible] = useState(true);
+  hideSplashScreen = () => {
+    setSplashScreenVisible(false);
+  };
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2500,
+      useNativeDriver: true,
+    }).start();
+  };
+  useEffect(() => {
+    fadeIn();
+    setTimeout(function () {
+      hideSplashScreen();
+    }, 5000);
+  });
+  let splashScreen = (
+    <View style={styles.splashScreenRootView}>
+      <StatusBar animated={true} hidden={true} />
+      <View style={styles.splashScreenChildView}>
+        <Animated.View
+          style={{
+            // Bind opacity to animated value
+            opacity: fadeAnim,
+          }}>
+          <Text style={styles.textSplash}>Welcome To</Text>
+          <Image source={require('./src/assets/img/arc_logo.png')} />
+        </Animated.View>
+      </View>
+    </View>
+  );
+  return (
+    <NavigationContainer style={styles.navigationContainer}>
+      <Navigator headerMode={'none'}>
+        {/* Testing Screen For Navigation Only */}
+        <Screen name="navigation-testing" component={Test} />
+        {/* Home Screen */}
+        <Screen name="Home" component={Home} />
+        {/* Auth Screen */}
+        <Screen name="Login" component={Login} />
+        {/* Topup Screen */}
+        <Screen name="TopUp" component={TopUp} />
+      </Navigator>
+      {splashScreenVisible === true ? splashScreen : null}
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create( {   
-        navigationContainer: {
-          backgroundColor: '#F9F9F9',
-        },
-        splashScreenRootView: {  
-            justifyContent: 'center',  
-            flex:1,   
-            position: 'absolute',  
-            width: '100%',  
-            height: '100%',
-          },  
-       
-        splashScreenChildView: {  
-            justifyContent: 'center',  
-            alignItems: 'center',  
-            backgroundColor: '#FFFFFF',  
-            flex:1,  
-        },
-        textSplash : {
-            fontFamily: 'NunitoSans-ExtraLightItalic',
-            textAlign: 'center',
-            fontSize: 20,
-            color: "#6379F4"
-        }
-    });
+const styles = StyleSheet.create({
+  navigationContainer: {
+    backgroundColor: '#F9F9F9',
+  },
+  splashScreenRootView: {
+    justifyContent: 'center',
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
 
-export default App
+  splashScreenChildView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+  },
+  textSplash: {
+    fontFamily: 'NunitoSans-ExtraLightItalic',
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#6379F4',
+  },
+});
+
+export default App;
