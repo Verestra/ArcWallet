d749404003d0f07cca64177690ccc4a6f5f0ffca
litha <lithaoctadelistia@gmail.com>
2021-06-10 13:54:08 +0700

    personal Information

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -7,11 +7,10 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-import {Card, CardItem, Left, Right, Body} from 'native-base';
 
-function Profile() {
-    const username = "Robert Chandler";
-    const usernumber = "+62 813-9387-7946";
+function Profile({...props}) {
+  const username = 'Robert Chandler';
+  const usernumber = '+62 813-9387-7946';
   return (
     <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
       <View style={styles.header}>
@@ -33,21 +32,23 @@ function Profile() {
           <Text style={styles.textEdit}>{usernumber}</Text>
         </View>
         <View style={{marginTop: 40}}>
-          <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.card}
+            onPress={() => props.navigation.navigate('PersonalInformation')}>
             <Text style={styles.textInCard}>Personal Information</Text>
-          </View>
-          <View style={styles.card}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
             <Text style={styles.textInCard}>Change Password</Text>
-          </View>
-          <View style={styles.card}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
             <Text style={styles.textInCard}>Change PIN</Text>
-          </View>
-          <View style={styles.card}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
             <Text style={styles.textInCard}>Notification</Text>
-          </View>
-          <View style={styles.card}>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
             <Text style={styles.textInCard}>Logout</Text>
-          </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </View>
@@ -66,38 +67,38 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
   },
-  profilePic:{
-    marginBottom:20,
+  profilePic: {
+    marginBottom: 20,
   },
-  textEdit:{
-fontFamily: 'NunitoSans',
-fontSize: 16,
-color: '#7A7886',
-marginBottom:20,
+  textEdit: {
+    fontFamily: 'NunitoSans',
+    fontSize: 16,
+    color: '#7A7886',
+    marginBottom: 20,
   },
-  textUsername:{
-fontFamily: 'NunitoSans',
-fontWeight: 'bold',
-fontSize: 24,
-color: '#4D4B57',
+  textUsername: {
+    fontFamily: 'NunitoSans',
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#4D4B57',
   },
-  card:{
-      backgroundColor:'#E5E8ED',
-      width:'90%',
-      height:60,
-      borderRadius:10,
-      alignSelf:'center',
-      padding:10,
-      marginBottom:20,
-      paddingLeft:20
+  card: {
+    backgroundColor: '#E5E8ED',
+    width: '90%',
+    height: 60,
+    borderRadius: 10,
+    alignSelf: 'center',
+    padding: 10,
+    marginBottom: 20,
+    paddingLeft: 20,
   },
-  textInCard:{
-fontFamily: 'NunitoSans',
-fontWeight: 'bold',
-fontSize: 17,
-lineHeight: 28,
-color: '#4D4B57',
-textAlignVertical:'center',
-  }
+  textInCard: {
+    fontFamily: 'NunitoSans',
+    fontWeight: 'bold',
+    fontSize: 17,
+    lineHeight: 28,
+    color: '#4D4B57',
+    textAlignVertical: 'center',
+  },
 });
 export default Profile;
